7a61e299c179a643492ac8cb937aca364fcd5de5
Arindam Paul (sWorks) <108253909+arindam-sworks@users.noreply.github.com>
2025-03-12 23:45:16 +0530

    Complete ticket 101 tasks - FormPage

--- src/lib/validationRules.ts
+++ src/lib/validationRules.ts
@@ -1,7 +1,46 @@
 export const validationRules = {
-        firstName: {
-          required: true,
-          pattern: /^[A-Za-z]{2,10}$/,
-          message: 'First name must be 2-10 letters only'
-        }
-}
+  firstName: {
+    required: true,
+    pattern: /^[A-Za-z]{2,50}$/,
+    message: 'First name must be 2-50 letters only',
+  },
+  lastName: {
+    required: true,
+    pattern: /^[A-Za-z]{2,50}$/,
+    message: 'Last name must be 2-50 letters only',
+  },
+  supervisorEmail: {
+    required: true,
+    pattern: /^[a-zA-Z0-9._%+-]+@the4d\.ca$/,
+    message: 'Must be a valid @the4d.ca email',
+  },
+  employeeId: {
+    required: true,
+    pattern: /^[A-Z]{3}-\d{5}$/,
+    message: 'Format: ABC-12345',
+  },
+  phoneNumber: {
+    required: true,
+    pattern: /^\+1 \(\d{3}\) \d{3}-\d{4}$/,
+    message: 'Format: +1 (555) 555-5555',
+  },
+  salary: {
+    required: true,
+    pattern: /^[1-9]\d*$/,
+    message: 'Must be a positive number',
+  },
+  startDate: {
+    required: true,
+    message: 'Start date is required',
+  },
+  costCenter: {
+    required: true,
+    pattern: /^[A-Z]{2}-\d{3}-[A-Z]{3}$/,
+    message: 'Format: AB-123-ABC',
+  },
+  projectCode: {
+    required: true,
+    pattern: /^PRJ-\d{4}-\d{3}$/,
+    message: 'Format: PRJ-YEAR-001',
+  },
+};
